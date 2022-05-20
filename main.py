3c5dc1d0bd27011d3cbe155171d7df2b56fec865
Kevin Qi <kevin.qi@berkeley.edu>
2022-05-20 13:09:40 -0700

    Add remind command

--- main.py
+++ main.py
@@ -2,14 +2,8 @@ import discord
 from dotenv import load_dotenv
 import os
 
-from cogs.listener import Listener
-
 
 load_dotenv()
-
-DICE_SERVER_ID = 783787350521806868
-TEST_SERVER_ID = 977045406787702807
-
 
 intents = discord.Intents.default()
 intents.members=True
@@ -19,15 +13,13 @@ intents.message_content=True
 bot = discord.Bot(intents=intents)
 
 
+cogs_list = [
+    'listener',
+    'reminder',
+]
 
-@bot.event
-async def on_ready():
-    print(f"We have logged in as {bot.user}")
+for cog in cogs_list:
+    bot.load_extension(f'cogs.{cog}')
 
-@bot.slash_command(guild_ids=[DICE_SERVER_ID, TEST_SERVER_ID])
-async def hello(ctx):
-    await ctx.respond("Hello!")
-
-bot.add_cog(Listener(bot))
 
 bot.run(os.getenv('BOT_TOKEN'))
